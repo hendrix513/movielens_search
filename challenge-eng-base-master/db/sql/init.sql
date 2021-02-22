06bc1da32b35662c90d287cee62c0d3eb7f867a6
Michael Schwartz <michael.schwartz@gridunity.com>
2021-02-22 18:09:22 -0500

    updating db schema to match with MovieLens data

--- challenge-eng-base-master/db/sql/init.sql
+++ challenge-eng-base-master/db/sql/init.sql
@@ -1,5 +1,6 @@
 USE challenge;
 
-CREATE TABLE test(col VARCHAR(10));
-
-INSERT INTO test(col) VALUES('ok');
+CREATE TABLE links(movieId INT, imdbId INT, tmdbId INT);
+CREATE TABLE tags(userId INT, movieId INT, tag VARCHAR(200), timestamp INT);
+CREATE TABLE ratings(userId INT, movieId INT, rating INT, timestamp INT);
+CREATE TABLE movies(movieId INT, title VARCHAR(200), genres VARCHAR(200));
