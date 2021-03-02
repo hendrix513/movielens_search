b065730c8eb8ac85b2ee920f62511d0c84114d3e
Michael Schwartz <michael.schwartz@gridunity.com>
2021-03-02 09:52:43 -0500

    adding tests

--- challenge-eng-base-master/backend-python/app.py
+++ challenge-eng-base-master/backend-python/app.py
@@ -42,7 +42,8 @@ def search_movies():
     page = int(request_args.get('page'))
 
     where_clauses = []
-    from_clause = 'movies m join ratings r on m.movieId=r.movieId join links l on l.movieId=m.movieId'
+    from_clause = 'movies m left outer join ratings r on m.movieId=r.movieId left outer' \
+                  ' join links l on l.movieId=m.movieId'
 
     if tag_user_id or tag:
         from_clause = '{} {}'.format(from_clause, ' join tags t on t.movieId=m.movieId')
@@ -51,7 +52,7 @@ def search_movies():
             where_clauses.append('t.userId={}'.format(tag_user_id))
 
         if tag:
-            where_clauses.append('t.tag={}'.format(tag))
+            where_clauses.append('t.tag=\'{}\''.format(tag))
 
     if rating_user_id:
         where_clauses.append('m.movieId in (SELECT DISTINCT'
@@ -67,21 +68,18 @@ def search_movies():
     if movie_id:
         where_clauses.append('m.movieId={}'.format(movie_id))
 
-    if tag:
-        where_clauses.append('t.tag={}'.format(tag))
-
     where_clause = ' WHERE {}'.format(' AND '.join(where_clauses)) if where_clauses else ''
     query = 'SELECT m.title, m.genres, AVG(r.rating), l.imdbId, l.tmdbId FROM {}{} GROUP BY' \
-            ' m.movieId LIMIT {} OFFSET {}'.format(from_clause, where_clause,
+            ' m.movieId ORDER BY m.movieId ASC LIMIT {} OFFSET {}'.format(from_clause, where_clause,
                                                    limit, (page-1)*limit)
 
     with db.cursor() as cur:
         cur.execute(query)
         return flask.jsonify(
             {'data':
-                 [[title, genres.strip().split('|'), float(avg_rating),
-                   'www.imdb.com/title/{}'.format(imdb_id),
-                   'www.themoviedb.org/movie/{}'.format(tmdb_id)]
+                 [[title, genres.strip().split('|'), float(avg_rating) if avg_rating else None,
+                   'www.imdb.com/title/{}'.format(imdb_id) if imdb_id else None,
+                   'www.themoviedb.org/movie/{}'.format(tmdb_id) if tmdb_id else None]
                   for title, genres, avg_rating, imdb_id, tmdb_id in cur.fetchall()]})
 
 
--- challenge-eng-base-master/backend-python/test_app.py
+++ challenge-eng-base-master/backend-python/test_app.py
@@ -1,3 +1,4 @@
+import json
 import os
 import tempfile
 
@@ -8,7 +9,7 @@ from .app import app, db
 
 @pytest.fixture
 def client():
-    print(app, dir(app))
+    #print(app, dir(app))
     #db_fd, app.config['DATABASE'] = tempfile.mkstemp()
     app.config['TESTING'] = True
 
@@ -25,28 +26,67 @@ def client():
 @pytest.fixture
 def load_data():
     with db.cursor() as cur:
-        cur.execute('INSERT INTO movies VALUES (1, \'movie 1\', \'romance|horror\')')
-        cur.execute('INSERT INTO movies VALUES (2, \'movie 2\', \'romance|horror\')')
-        cur.execute('INSERT INTO movies VALUES (3, \'movie 3\', \'romance|horror\')')
+        cur.execute('TRUNCATE movies')
+        cur.execute('TRUNCATE links')
+        cur.execute('TRUNCATE tags')
+        cur.execute('TRUNCATE ratings')
+
+        cur.execute('INSERT INTO movies VALUES (1, \'movie1\', \'drama|romance\r\')')
+        cur.execute('INSERT INTO movies VALUES (2, \'movie2\', \'romance|horror\r\')')
+        cur.execute('INSERT INTO movies VALUES (3, \'movie3\', \'comedy\r\')')
+        cur.execute('INSERT INTO movies VALUES (4, \'movie4\', \'thriller\')')
 
         cur.execute('INSERT INTO links VALUES (1, 101, 201)')
         cur.execute('INSERT INTO links VALUES (2, 102, 202)')
         cur.execute('INSERT INTO links VALUES (3, 103, 203)')
 
-        cur.execute('INSERT INTO tags VALUES (1, 101, 201)')
-        cur.execute('INSERT INTO links VALUES (2, 102, 202)')
-        cur.execute('INSERT INTO links VALUES (3, 103, 203)')
+        cur.execute('INSERT INTO tags VALUES (1, 1, \'tag1\', 0)')
+        cur.execute('INSERT INTO tags VALUES (2, 3, \'tag1\', 0)')
+        cur.execute('INSERT INTO tags VALUES (1, 2, \'tag2\', 0)')
 
+        cur.execute('INSERT INTO ratings VALUES (1, 1, 7, 0)')
+        cur.execute('INSERT INTO ratings VALUES (1, 1, 9, 0)')
+        cur.execute('INSERT INTO ratings VALUES (2, 3, 6, 0)')
 
         db.commit()
 
 
 def test_search(client, load_data):
-    with db.cursor() as cur:
-        cur.execute('SELECT COUNT(*) FROM movies')
-        print(cur.fetchall())
-        db.commit()
+    movies_exp_return = [['movie1', ['drama', 'romance'], 8.0, 'www.imdb.com/title/101', 'www.themoviedb.org/movie/201'],
+                         ['movie2', ['romance', 'horror'], None, 'www.imdb.com/title/102', 'www.themoviedb.org/movie/202'],
+                         ['movie3', ['comedy'], 6.0, 'www.imdb.com/title/103', 'www.themoviedb.org/movie/203'],
+                         ['movie4', ['thriller'], None, None, None]
+                         ]
 
-    d = client.get('/search_movies?limit=10&page=1')
-    print(d)
-    print(d.data)
+    d = client.get('/search_movies?limit=2&page=1')
+    data = json.loads(d.data)['data']
+    assert data == movies_exp_return[:2]
+
+    d = client.get('/search_movies?limit=2&page=2')
+    data = json.loads(d.data)['data']
+    assert data == movies_exp_return[2:]
+
+    d = client.get('/search_movies?limit=10&page=1&movie_id=1')
+    data = json.loads(d.data)['data']
+    assert data == [movies_exp_return[0]]
+
+    d = client.get('/search_movies?limit=10&page=1&genre=romance')
+    data = json.loads(d.data)['data']
+    assert data == movies_exp_return[:2]
+
+    d = client.get('/search_movies?limit=10&page=1&genre=romance&title=ie2')
+    data = json.loads(d.data)['data']
+    assert data == [movies_exp_return[1]]
+
+    d = client.get('/search_movies?limit=10&page=1&tag=tag1')
+    data = json.loads(d.data)['data']
+    assert data == [movies_exp_return[0], movies_exp_return[2]]
+
+    d = client.get('/search_movies?limit=10&page=1&tag_user_id=1')
+    data = json.loads(d.data)['data']
+    assert data == movies_exp_return[:2]
+
+    d = client.get('/search_movies?limit=10&page=1&rating_user_id=2')
+    data = json.loads(d.data)['data']
+    assert data == [movies_exp_return[2]]
+
